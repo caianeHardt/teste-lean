fa76f8c15b10213cb28bda12f364c794d3ce8882
Caiane Hardt <caianehardt@Paulas-Air.lan>
2024-08-28 21:07:16 -0300

    Update docker and sql

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM node:18.0.0 as builder
 
 ENV PATH /app/node_modules/.bin:$PATH
-ENV NODE_ENV=development
+ENV NODE_ENV=dev
 
 # Defina o diretório de trabalho no contêiner
 WORKDIR /app
+
+# COPY db.sql /docker-entrypoint-initdb.d/
 
 # Copie o package.json e o package-lock.json
 COPY package*.json ./
--- db.sql
+++ db.sql
@@ -1,60 +1,60 @@
-CREATE TABLE public."user" (
-    id bigserial PRIMARY KEY,
-    "name" varchar NOT NULL,
-    cpf varchar NULL,
-    cnpj varchar NULL,
-    email varchar NOT NULL,
-    "password" varchar NOT NULL,
-    type_user varchar NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-    updated_at timestamptz NOT NULL DEFAULT NOW(),
-    CONSTRAINT user_pk PRIMARY KEY (id)
-
-);
-
 CREATE TABLE public.user_type (
-	id bigserial PRIMARY KEY,
+	id bigserial NOT NULL,
 	"type" varchar NOT NULL,
-	make_payment boolean NOT NULL,
-	created_at timestamptz NOT NULL DEFAULT NOW(),
-	updated_at timestamptz NOT NULL DEFAULT NOW(),
-	CONSTRAINT user_type_pk PRIMARY KEY (id)
+	make_payment bool NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	active bool NOT NULL,
+	CONSTRAINT user_type_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE public."user" (
+	id bigserial NOT NULL,
+	"name" varchar NOT NULL,
+	"documentNumber" varchar NOT NULL,
+	email varchar NOT NULL,
+	"password" varchar NOT NULL,
+	type_user int4 NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	CONSTRAINT user_pkey PRIMARY KEY (id),
+	CONSTRAINT user_type_fk FOREIGN KEY (type_user) REFERENCES public.user_type(id)
+);
+CREATE UNIQUE INDEX user_documentnumber_idx ON public."user" USING btree ("documentNumber");
+
 
 CREATE TABLE public.account (
-	id uuid DEFAULT gen_random_uuid() NOT NULL,
-	account_owner int NOT NULL,
-	active boolean NOT NULL,
-	balance decimal NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-	updated_at timestamptz NOT NULL DEFAULT NOW(),
-	CONSTRAINT account_pk PRIMARY KEY (id)
-    CONSTRAINT account_fk FOREIGN KEY (account_owner)
-        REFERENCES public."user" (id)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE public.transaction (
 	id bigserial NOT NULL,
-	amount_paid decimal NOT NULL,
-	transfer_sent_to int NOT NULL,
-    transfer_sent_from int NOT NULL,
-    created_at timestamptz NOT NULL DEFAULT NOW(),
-	updated_at timestamptz NOT NULL DEFAULT NOW(),
-	CONSTRAINT transactions_pk PRIMARY KEY (id),
-    CONSTRAINT transaction_fk FOREIGN KEY (payee)
-        REFERENCES public."account" (id)
-        CONSTRAINT transaction_fk FOREIGN KEY (payer)
-        REFERENCES public."account" (id)
+	account_owner int4 NOT NULL,
+	active bool NOT NULL,
+	balance numeric NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	CONSTRAINT account_pk PRIMARY KEY (id),
+	CONSTRAINT account_fk FOREIGN KEY (account_owner) REFERENCES public."user"(id) ON DELETE CASCADE
 );
 
-INSERT INTO public."user" (
-    "name", cpf, cnpj, email, "password", type_user
-    ) 
-    VALUES ('Usuario Teste', NULL, NULL, 'teste@example.com', '123456', 'client');
-    INSERT INTO public."user" ("name", cpf, cnpj, email, "password", type_user) 
-    VALUES ('Usuario Cliente Teste', '588.879.670-08', NULL, 'teste@example.com', '123456', 'client'),
-    ('Usuario Lojista Teste', NULL, '12.345.678/0001-90', 'test1@exemple.com','pass1', 'account_manager'	);
+
+CREATE TABLE public."transaction" (
+	id bigserial NOT NULL,
+	amount_paid numeric NOT NULL,
+	payee int4 NOT NULL,
+	payer int4 NOT NULL,
+	created_at timestamptz DEFAULT now() NOT NULL,
+	updated_at timestamptz DEFAULT now() NOT NULL,
+	CONSTRAINT transaction_pk PRIMARY KEY (id),
+	CONSTRAINT payee_fk FOREIGN KEY (payee) REFERENCES public.account(id),
+	CONSTRAINT payer_fk FOREIGN KEY (payer) REFERENCES public.account(id)
+);
 
 INSERT INTO public.user_type (type, active, make_payment)
-VALUES ('client', true, true), ('account_manager', true, false);
+VALUES ('client', true, true), 
+        ('account_manager', true, false);
+
+INSERT INTO public."user" ("name", "documentNumber", email, "password", type_user) 
+VALUES ('Usuario Cliente Teste', '588.879.670-08', 'teste@example.com', '123456', 1),
+		('Usuario Lojista Teste', '12.345.678/0001-90', 'test1@exemple.com','pass1', 2);
+
+INSERT INTO public.account ("account_owner", "active", balance, active) 
+VALUES ('1', 'true', '100,00', true),
+		('2', 'true', '100,00', true);
